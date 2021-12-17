1adcc341f590aba1ae0cb1fea5a5958f37511161
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2021-12-17 12:15:58 +0100

    Add tamplate to app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('base.html')
+
+
+if __name__ == "__main__":
+  app.run()
